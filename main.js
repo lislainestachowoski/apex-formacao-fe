6c807acbda08cb6e8c8ca6f33d74b5b2f186f8e2
Lislaine Stachowoski <lislaines@gmail.com>
2018-08-09 22:36:00 -0300

    Aula 01 HTML

---
+++ main.js
@@ -0,0 +1,13 @@
+function teste(){
+    var element = document.getElementsByTagName('div');
+
+    element[0].innerHTML = 'Hello word';
+
+    return element;
+};
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
